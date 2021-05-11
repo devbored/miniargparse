d351b0f4b72f51098bb4aa11a00b3dceb051d265
Austin Annestrand <austin.annestrand@gmail.com>
2021-05-11 02:39:46 -0500

    Added option parsing loop

--- miniargparse.h
+++ miniargparse.h
@@ -1,18 +1,79 @@
 #ifndef MINIARGPARSE_H
 #define MINIARGPARSE_H
 
-#define MINIARGPARSE_OPT(storeValue, _shortName, _longName, _valCount, _description)    \
-    struct {                                                                            \
-        const char *shortName;                                                          \
-        const char *longName;                                                           \
-        unsigned int valCount;                                                          \
-        const char *description;                                                        \
-        unsigned short used;                                                            \
-    } storeValue = {                                                                    \
-        .shortName = _shortName,                                                        \
-        .longName = _longName,                                                          \
-        .valCount = _valCount,                                                          \
-        .description = _description,                                                    \
-        .used = 0 };
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct MiniargparseOpt {
+    const char *shortName;
+    const char *longName;
+    bool hasValue;
+    const char *description;
+    bool used;
+    const char *value;
+    bool hasErrVal;
+    const char *errValMsg;
+} miniargparseOpt;
+
+#define USED(val) (!(val == NULL) && !(strcmp(val, "") == 0))
+#define STR_MATCH(str1, str2) (strcmp(str1, str2) == 0)
+#define SUBSTR_MATCH(str1, str2) (strstr(str1, str2) != NULL)
+
+// Global error strings
+const char *g_errMalformedOptionVal = "Malformed --<option>=<value> : ";
+const char *g_errOptValEndOfArgv = "Option already at end of argv";
+const char *g_errValIsOpt = "Value has option syntax (i.e. -, --) : ";
+
+#define MINIARGPARSE_INIT(argc, arvg)   \
+    int MINIARGPARSE_##argc = argc;     \
+    char **MINIARGPARSE_##argv = argv;  \
+    do {} while(0)
+
+#define MINIARGPARSE_OPT(storeValue, shortName, longName, hasVal, description)              \
+    miniargparseOpt storeValue = { shortName, longName, hasVal, description, 0, "", 0, "" };\
+    do {                                                                                    \
+        for (size_t i=1; i<MINIARGPARSE_argc; ++i) {                                        \
+            int isLongOpt = 0;                                                              \
+            if (USED(shortName) && STR_MATCH("-" shortName, MINIARGPARSE_argv[i])) {        \
+                storeValue.used = 1;                                                        \
+            }                                                                               \
+            else if (USED(longName) && SUBSTR_MATCH(MINIARGPARSE_argv[i], "--" longName)) { \
+                isLongOpt = 1;                                                              \
+                storeValue.used = 1;                                                        \
+            }                                                                               \
+            if (storeValue.used && hasVal == 1) {                                           \
+                if (isLongOpt) {                                                            \
+                    char *val;                                                              \
+                    size_t offset;                                                          \
+                    val = strchr(MINIARGPARSE_argv[i], '=');                                \
+                    if (val == NULL) {                                                      \
+                        storeValue.hasErrVal = 1;                                           \
+                        storeValue.errValMsg = g_errMalformedOptionVal;                     \
+                        storeValue.value = MINIARGPARSE_argv[i];                            \
+                    }                                                                       \
+                    else {                                                                  \
+                        offset = (int)(val - MINIARGPARSE_argv[i]);                         \
+                        storeValue.value = &MINIARGPARSE_argv[i][offset+1];                 \
+                    }                                                                       \
+                }                                                                           \
+                else {                                                                      \
+                    if ((i+1) >= MINIARGPARSE_argc) {                                       \
+                        storeValue.hasErrVal = 1;                                           \
+                        storeValue.errValMsg = g_errOptValEndOfArgv;                        \
+                    }                                                                       \
+                    else if (MINIARGPARSE_argv[i+1][0] == '-') {                            \
+                        storeValue.hasErrVal = 1;                                           \
+                        storeValue.errValMsg = g_errValIsOpt;                               \
+                        storeValue.value = MINIARGPARSE_argv[i+1];                          \
+                    }                                                                       \
+                    else {                                                                  \
+                        storeValue.value = MINIARGPARSE_argv[i+1];                          \
+                    }                                                                       \
+                    ++i;                                                                    \
+                }                                                                           \
+                break;                                                                      \
+            }                                                                               \
+        }                                                                                   \
+    } while(0)
 
 #endif // MINIARGPARSE_H
--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -8,11 +8,31 @@
 #include <gtest/gtest.h>
 #include "miniargparse.h"
 
+void printOptItems(miniargparseOpt opt) {
+    std::cout << "Opt fields:\n---\n";
+    std::cout << "Short name : " << opt.shortName << std::endl;
+    std::cout << "Long name : " << opt.longName << std::endl;
+    std::cout << "Has value? : " << opt.hasValue << std::endl;
+    std::cout << "Description : " << opt.description << std::endl;
+    std::cout << "Used? : " << opt.used << std::endl;
+    std::cout << "Value : " << opt.value << std::endl;
+    std::cout << "Has value error? : " << opt.hasErrVal << std::endl;
+    std::cout << "Value error : " << opt.errValMsg << std::endl;
+    std::cout << "---\n\n";
+}
+
 TEST(miniargparse, test_works) {
 
+    int argc = 4;
+    char *argv[] = {0, "-b", "--verbose", "-d"};
+
+    MINIARGPARSE_INIT(argc, argv);
     MINIARGPARSE_OPT(verbose, "v", "verbose", 0, "Enable verbose message.");
     MINIARGPARSE_OPT(help, "h", "help", 0, "Print out help message.");
+    MINIARGPARSE_OPT(data, "d", "data", 1, "Test data.");
 
     // Print out descriptions of each arg
-    std::cout << verbose.used << std::endl;
+    printOptItems(verbose);
+    printOptItems(help);
+    printOptItems(data);
 }
