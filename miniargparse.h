2cbf893aeac3c7c68fa2f00afd9bed274171edf5
Austin Annestrand <austin.annestrand@gmail.com>
2021-05-12 23:52:59 -0500

    Proper funtion to parse opts now, added pos. args. getter function

--- miniargparse.h
+++ miniargparse.h
@@ -2,78 +2,151 @@
 #define MINIARGPARSE_H
 
 #include <string.h>
-#include <stdbool.h>
+#include <assert.h>
+
+typedef struct MiniargparseInfoBits {
+    unsigned int hasValue   : 1;
+    unsigned int hasErr     : 1;
+    unsigned int used       : 1;
+} miniargparseInfoBits;
 
 typedef struct MiniargparseOpt {
     const char *shortName;
     const char *longName;
-    bool hasValue;
     const char *description;
-    bool used;
     const char *value;
-    bool hasErrVal;
     const char *errValMsg;
+    unsigned int index;
+    miniargparseInfoBits infoBits;
+    struct MiniargparseOpt *next;
 } miniargparseOpt;
 
-#define USED(val) (!(val == NULL) && !(strcmp(val, "") == 0))
 #define STR_MATCH(str1, str2) (strcmp(str1, str2) == 0)
 #define SUBSTR_MATCH(str1, str2) (strstr(str1, str2) != NULL)
+#define STR_USED(val) (!(val == NULL) && !(strcmp(val, "") == 0))
 
 // Global error strings
-const char *g_errMalformedOptionVal = "Malformed --<option>=<value> : ";
-const char *g_errOptValEndOfArgv = "Option already at end of argv";
-const char *g_errValIsOpt = "Value has option syntax (i.e. -, --) : ";
+enum miniargparseErrIndexes {
+    ERR_MALFORMED_OPT_VAL,
+    ERR_OPT_VAL_END_ARGV,
+    ERR_VAL_IS_OPT
+};
+static const char *g_miniargparseErrStrs[] = {
+    "Malformed --<option>=<value>",
+    "Option already at end of argv",
+    "Value has option syntax (i.e. -, --)"
+};
 
-#define MINIARGPARSE_INIT(argc, arvg)   \
-    int MINIARGPARSE_##argc = argc;     \
-    char **MINIARGPARSE_##argv = argv;  \
-    do {} while(0)
+// Global ptr to head of options
+static miniargparseOpt *g_miniargparseHead;
 
-#define MINIARGPARSE_OPT(storeValue, shortName, longName, hasVal, description)              \
-    miniargparseOpt storeValue = { shortName, longName, hasVal, description, 0, "", 0, "" };\
-    do {                                                                                    \
-        for (size_t i=1; i<MINIARGPARSE_argc; ++i) {                                        \
-            int isLongOpt = 0;                                                              \
-            if (USED(shortName) && STR_MATCH("-" shortName, MINIARGPARSE_argv[i])) {        \
-                storeValue.used = 1;                                                        \
-            }                                                                               \
-            else if (USED(longName) && SUBSTR_MATCH(MINIARGPARSE_argv[i], "--" longName)) { \
-                isLongOpt = 1;                                                              \
-                storeValue.used = 1;                                                        \
-            }                                                                               \
-            if (storeValue.used && hasVal == 1) {                                           \
-                if (isLongOpt) {                                                            \
-                    char *val;                                                              \
-                    size_t offset;                                                          \
-                    val = strchr(MINIARGPARSE_argv[i], '=');                                \
-                    if (val == NULL) {                                                      \
-                        storeValue.hasErrVal = 1;                                           \
-                        storeValue.errValMsg = g_errMalformedOptionVal;                     \
-                        storeValue.value = MINIARGPARSE_argv[i];                            \
-                    }                                                                       \
-                    else {                                                                  \
-                        offset = (int)(val - MINIARGPARSE_argv[i]);                         \
-                        storeValue.value = &MINIARGPARSE_argv[i][offset+1];                 \
-                    }                                                                       \
-                }                                                                           \
-                else {                                                                      \
-                    if ((i+1) >= MINIARGPARSE_argc) {                                       \
-                        storeValue.hasErrVal = 1;                                           \
-                        storeValue.errValMsg = g_errOptValEndOfArgv;                        \
-                    }                                                                       \
-                    else if (MINIARGPARSE_argv[i+1][0] == '-') {                            \
-                        storeValue.hasErrVal = 1;                                           \
-                        storeValue.errValMsg = g_errValIsOpt;                               \
-                        storeValue.value = MINIARGPARSE_argv[i+1];                          \
-                    }                                                                       \
-                    else {                                                                  \
-                        storeValue.value = MINIARGPARSE_argv[i+1];                          \
-                    }                                                                       \
-                    ++i;                                                                    \
-                }                                                                           \
-                break;                                                                      \
-            }                                                                               \
-        }                                                                                   \
+#define MINIARGPARSE_OPT(storeVal, shortName, longName, hasValue, description)                          \
+    assert(!SUBSTR_MATCH("-", shortName) &&                                                             \
+        "[miniargparse]: ERROR - The '-' character is not permitted as a shortName");                   \
+    assert(strlen(shortName) == 1 &&                                                                    \
+        "[miniargparse]: ERROR - The shortName string can only have 1 character");                      \
+    assert(!STR_MATCH("", shortName) && !STR_MATCH("", longName) &&                                     \
+        "[miniargparse]: ERROR - Both shortName and longName are empty");                               \
+    miniargparseOpt storeVal =                                                                          \
+        { "-" shortName, "--" longName, description, "", "", 0, { hasValue, 0, 0 }, NULL };             \
+    do {                                                                                                \
+        if (g_miniargparseHead == NULL) {                                                               \
+            g_miniargparseHead = &storeVal;                                                             \
+        }                                                                                               \
+        else {                                                                                          \
+            miniargparseOpt *tmp = g_miniargparseHead;                                                  \
+            while (tmp->next != NULL) {                                                                 \
+                tmp = tmp->next;                                                                        \
+            }                                                                                           \
+            tmp->next = &storeVal;                                                                      \
+        }                                                                                               \
     } while(0)
+
+static int miniargparseParse(int argc, char *argv[]) {
+    miniargparseOpt *tmp;
+    int firstUnknownOpt = 0;
+    for (size_t i=1; i<argc; ++i) {
+        // Check if arg is not a option-type
+        if (argv[i][0] != '-') { continue; }
+
+        int isLongOpt = 0;
+        int validOpt = 0;
+        tmp = g_miniargparseHead;
+
+        // Look for opt in opts list
+        while (tmp != NULL) {
+            if (STR_USED(tmp->shortName) && STR_MATCH(tmp->shortName, argv[i])) {
+                tmp->infoBits.used = 1;
+                tmp->index = i;
+                validOpt = 1;
+            }
+            else if (STR_USED(tmp->longName) && SUBSTR_MATCH(argv[i], tmp->longName)) {
+                isLongOpt = 1;
+                tmp->infoBits.used = 1;
+                tmp->index = i;
+                validOpt = 1;
+            }
+            if (tmp->infoBits.used && tmp->infoBits.hasValue) {
+                if (isLongOpt) {
+                    char *val;
+                    size_t offset;
+                    val = strchr(argv[i], '=');
+                    if (val == NULL) {
+                        tmp->infoBits.hasErr = 1;
+                        tmp->errValMsg = g_miniargparseErrStrs[ERR_MALFORMED_OPT_VAL];
+                        tmp->value = argv[i];
+                    }
+                    else {
+                        offset = (int)(val - argv[i]);
+                        tmp->value = &argv[i][offset+1];
+                    }
+                }
+                else {
+                    if ((i+1) >= argc) {
+                        tmp->infoBits.hasErr = 1;
+                        tmp->errValMsg = g_miniargparseErrStrs[ERR_OPT_VAL_END_ARGV];
+                    }
+                    else if (argv[i+1][0] == '-') {
+                        tmp->infoBits.hasErr = 1;
+                        tmp->errValMsg = g_miniargparseErrStrs[ERR_VAL_IS_OPT];
+                        tmp->value = argv[i+1];
+                    }
+                    else {
+                        tmp->value = argv[i+1];
+                    }
+                    ++i;
+                }
+                break;
+            }
+            tmp = tmp->next;
+        }
+
+        // Track first occurance of unknown opt
+        if (!validOpt && firstUnknownOpt == 0) { firstUnknownOpt = i; }
+    }
+    return firstUnknownOpt;
+}
+
+int miniargparseGetPositionalArg(int argc, char *argv[], size_t argvOffset) {
+    size_t i;
+    for (i=argvOffset+1; i<argc; ++i) {
+        // Skip if opt-type value
+        if (argv[i][0] == '-') { continue; }
+
+        // Otherwise check if arg is opt-value type or not
+        int isOptValue = 0;
+        miniargparseOpt *tmp = g_miniargparseHead;
+        while (tmp != NULL) {
+            if (tmp->infoBits.used && tmp->infoBits.hasValue && tmp->index == i-1) {
+                isOptValue = 1;
+            }
+            tmp = tmp->next;
+        }
+
+        // Found positional arg
+        if (!isOptValue) { return i; }
+    }
+    return 0;
+}
 
 #endif // MINIARGPARSE_H
--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -8,31 +8,26 @@
 #include <gtest/gtest.h>
 #include "miniargparse.h"
 
-void printOptItems(miniargparseOpt opt) {
-    std::cout << "Opt fields:\n---\n";
-    std::cout << "Short name : " << opt.shortName << std::endl;
-    std::cout << "Long name : " << opt.longName << std::endl;
-    std::cout << "Has value? : " << opt.hasValue << std::endl;
-    std::cout << "Description : " << opt.description << std::endl;
-    std::cout << "Used? : " << opt.used << std::endl;
-    std::cout << "Value : " << opt.value << std::endl;
-    std::cout << "Has value error? : " << opt.hasErrVal << std::endl;
-    std::cout << "Value error : " << opt.errValMsg << std::endl;
-    std::cout << "---\n\n";
-}
+TEST(miniargparse, test_valid_opts_basic) {
+    // Mock argv and argc
+    char *argv[] = {
+        "<EXECUTABLE>",
+        "-h",
+        "--verbose",
+        "-d",
+        "6",
+    };
+    int argc = sizeof(argv) / sizeof(char*);
 
-TEST(miniargparse, test_works) {
+    // Add and parse opts
+    MINIARGPARSE_OPT(help, "h", "help", 0, "Print out help and exit");
+    MINIARGPARSE_OPT(verbose, "v", "verbose", 0, "Add verbosity");
+    MINIARGPARSE_OPT(data, "d", "data", 1, "Test data-value option");
+    int unknownOpt = miniargparseParse(argc, argv);
+    EXPECT_EQ(unknownOpt, 0);
 
-    int argc = 4;
-    char *argv[] = {0, "-b", "--verbose", "-d"};
-
-    MINIARGPARSE_INIT(argc, argv);
-    MINIARGPARSE_OPT(verbose, "v", "verbose", 0, "Enable verbose message.");
-    MINIARGPARSE_OPT(help, "h", "help", 0, "Print out help message.");
-    MINIARGPARSE_OPT(data, "d", "data", 1, "Test data.");
-
-    // Print out descriptions of each arg
-    printOptItems(verbose);
-    printOptItems(help);
-    printOptItems(data);
+    // Basic validation of parsed opts
+    EXPECT_EQ(help.infoBits.used, 1);
+    EXPECT_EQ(verbose.infoBits.used, 1);
+    EXPECT_EQ(data.infoBits.used, 1);
 }
